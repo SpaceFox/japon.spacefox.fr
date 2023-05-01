e036d5b87cb4acdcbab8d0331aa27effea67e160
SpaceFox <ujueseo.yeou@gmail.com>
2023-05-01 19:09:57 +0200

    MàJ dépendances

--- src/main/kotlin/fr/spacefox/japon/Resource.kt
+++ src/main/kotlin/fr/spacefox/japon/Resource.kt
@@ -5,11 +5,11 @@ import fr.spacefox.japon.data.Page
 import fr.spacefox.japon.data.Place
 import io.quarkus.qute.Template
 import io.quarkus.qute.TemplateInstance
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.Produces
-import javax.ws.rs.core.MediaType
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
 
 @Path("/")
 class ListHtmlResource(val list: Template) {
